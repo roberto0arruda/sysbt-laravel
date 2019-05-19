eb6869f1f01b50da31782998491097189916727a
Roberto Arruda <roberto0arruda@hotmail.com>
2019-05-19 11:41:49 -0400

    edit view index finances

--- resources/views/admin/finance/index.blade.php
+++ resources/views/admin/finance/index.blade.php
@@ -26,8 +26,9 @@
                             <th></th>
                             <th>Produto</th>
                             <th>Parcela</th>
-                            <th>DT Vence</th>
+                            <th>DT Vencer</th>
                             <th>Valor</th>
+                            <th>Cliente</th>
                             <th>Recebeu em</th>
                             <th>Action</th>
                         </tr>
@@ -36,18 +37,19 @@
                         @foreach ($payments as $payment)
                         <tr data-entry-id="{{ $payment->id }}" @if ($payment->paid == 1) class="success" @endif >
                             <td>{{$payment->id}}</td>
-                            <td>{{$payment->product->title}}</td>
+                            <td>
+                                <a href="{{route('products.show', $payment->product->id)}}" class="btn btn-xs btn-info"><i class="fa fa-mobile" aria-hidden="true"></i> {{$payment->product->title}}</a>
+                            </td>
                             <td>{{$payment->parc}}</td>
                             <td>{{ date('d-m-Y', strtotime($payment->venciment) ) }}</td>
                             <td>R$ {{ number_format($payment->value, 2)}}</td>
+                            <td>{{$payment->product->client}}</td>
                             <td> @if ($payment->paid == 1) {{ date('d-m-Y H:m:s', strtotime($payment->updated_at)) }} @endif </td>
                             <td>
-                                {{-- <a href="{{route('products.show', $product->id)}}" class="btn btn-xs btn-info"><i class="fa fa-eye" aria-hidden="true"></i> Show</a> --}}
                                 @if ($payment->paid != 1)
                                     <a href="{{route('finances.edit', [$payment->id])}} " id="receber" class="btn btn-xs btn-danger"><i class="fa fa-money" aria-hidden="true"></i> receber</a>
                                 @else
                                     <label class="btn btn-xs btn-success"><i class="fa fa-thumbs-o-up" aria-hidden="true"></i> ok</label>
-                                    {{-- <p class="btn btn-xs btn-success"><i class="fa fa-thumbs-o-up" aria-hidden="true"></i> OK</p> --}}
                                 @endif
                             </td>
                         </tr>                        
