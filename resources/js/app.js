2c274e85e9f2ccafe9d81d522de451a3263fe293
Roberto Arruda <roberto0arruda@hotmail.com>
2019-10-08 22:01:49 -0400

    resolve node dependences

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,14 +7,6 @@
 
 require('./bootstrap');
 
-const swal = window.swal = require('sweetalert2');
-const toast = window.toast = swal.mixin({
-    toast: true,
-    position: 'top-end',
-    showConfirmButton: false,
-    timer: 2500
-});
-
 window.Vue = require('vue');
 
 /**
