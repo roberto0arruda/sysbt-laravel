6f4fdb7396c69a447358d87e09991e763db49d4f
Roberto Arruda <roberto0arruda@hotmail.com>
2021-02-13 20:27:53 -0400

    chore: change ForgotPasswordController@sendResetLinkEmail
    should verify if request has route to override reset password url

--- app/Unit/Auth/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Unit/Auth/Http/Controllers/Auth/ForgotPasswordController.php
@@ -3,7 +3,11 @@
 namespace App\Unit\Auth\Http\Controllers\Auth;
 
 use App\Support\Http\Controllers\Controller;
+use Illuminate\Auth\Notifications\ResetPassword as ResetPasswordNotification;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Validation\ValidationException;
 
 class ForgotPasswordController extends Controller
 {
@@ -19,4 +23,33 @@ class ForgotPasswordController extends Controller
     */
 
     use SendsPasswordResetEmails;
+
+    /**
+     * Send a reset link to the given user.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     * @throws ValidationException
+     */
+    public function sendResetLinkEmail(Request $request)
+    {
+        $this->validateEmail($request);
+
+        if ($request->has('route')) {
+            ResetPasswordNotification::createUrlUsing(function ($notifiable, $token) use ($request) {
+                return "{$request->get('route')}/{$token}";
+            });
+        }
+
+        // We will send the password reset link to this user. Once we have attempted
+        // to send the link, we will examine the response then see the message we
+        // need to show to the user. Finally, we'll send out a proper response.
+        $response = $this->broker()->sendResetLink(
+            $this->credentials($request)
+        );
+
+        return $response == Password::RESET_LINK_SENT
+            ? $this->sendResetLinkResponse($request, $response)
+            : $this->sendResetLinkFailedResponse($request, $response);
+    }
 }
